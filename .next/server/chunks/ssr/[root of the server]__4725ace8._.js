de0e764a6153c97d50c646203abf72a3d411aab8
zhaosir1 <401657872@qq.com>
2025-04-01 16:19:27 +0800

    修改bug

--- .next/server/chunks/ssr/[root of the server]__4725ace8._.js
+++ .next/server/chunks/ssr/[root of the server]__4725ace8._.js
@@ -153,7 +153,7 @@ function IntroduceMobile() {
                             children: [
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["default"], {
                                     className: "mr-[11px] absolute left-0 top-0 z-[-1]",
-                                    src: "/images/btnbag.png",
+                                    src: "/images/btnBag.png",
                                     alt: "",
                                     width: 110,
                                     height: 35
@@ -208,7 +208,7 @@ function IntroduceMobile() {
                             children: [
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["default"], {
                                     className: "mr-[11px] absolute left-0 top-0 z-[-1]",
-                                    src: "/images/btnbag.png",
+                                    src: "/images/btnBag.png",
                                     alt: "",
                                     width: 110,
                                     height: 35
--- components/mobile/IntroduceMobile.tsx
+++ components/mobile/IntroduceMobile.tsx
@@ -13,7 +13,7 @@ export default function IntroduceMobile() {
     <div className="flex justify-center">
       <a className="mr-[11px]" href="http://obs.alstarai.com/20241204/app-release.apk">
         <div className="w-[110px] h-[35px] relative">
-          <Image className="mr-[11px] absolute left-0 top-0 z-[-1]" src="/images/btnbag.png" alt="" width={110} height={35}></Image>
+          <Image className="mr-[11px] absolute left-0 top-0 z-[-1]" src="/images/btnBag.png" alt="" width={110} height={35}></Image>
           <div className="flex h-full items-center justify-center">
             <Image className="mr-[2px]" width={13} height={13} src="/images/android.png" alt=""></Image>
             <div className="text-[11px] font-[600]">{t.androidDownload}</div>
@@ -22,7 +22,7 @@ export default function IntroduceMobile() {
       </a>
       <a href="https://apps.apple.com/cn/app/uuinin/id6737213354">
         <div className="w-[110px] h-[35px] relative">
-          <Image className="mr-[11px] absolute left-0 top-0 z-[-1]" src="/images/btnbag.png" alt="" width={110} height={35}></Image>
+          <Image className="mr-[11px] absolute left-0 top-0 z-[-1]" src="/images/btnBag.png" alt="" width={110} height={35}></Image>
           <div className="flex h-full items-center justify-center">
             <Image className="mr-[2px]" width={13} height={13} src="/images/apple.png" alt=""></Image>
             <div className="text-[11px] font-[600]">{t.appStoreDownload}</div>
